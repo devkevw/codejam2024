e904e367c29b460021c714d9e67ef2341e87f752
zuhayr <zuhayrmahmood@yahoo.ca>
2024-11-23 14:40:07 -0500

    Updated Journal

--- backend/app/Journal.py
+++ backend/app/Journal.py
@@ -2,11 +2,13 @@ import model.JournalEntry as JournalEntry
 
 class Journal:
     def __init__(self, user_id):
-        self.entries = []
+        self.entries = [] # list of JournalEntry objects
         self.user_id = user_id
 
     def add_entry(self, entry: JournalEntry):
+        """Add a new entry to the journal."""
         self.entries.append(entry)
 
     def get_entries(self):
+        """Return a list of all entries for the user."""
         return self.entries
