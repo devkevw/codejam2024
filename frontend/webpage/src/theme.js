796613f3f5604ff620b9079ea9d57eca2e182172
erichugy <eric.huang3143@gmail.com>
2024-11-24 01:59:27 -0500

    adding theme and index

---
+++ frontend/webpage/src/theme.js
@@ -0,0 +1,39 @@
+// src/theme/theme.js
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#98CEED', // light blue
+    },
+    secondary: {
+      main: '#438fc2', // Orange
+    },
+    background: {
+      default: '#f9f9f9', // Light gray
+      paper: '#ffffff', // White for Paper components
+    },
+    text: {
+      primary: '#000000', // Dark text
+      secondary: '#CCCCCC', // Light gray text
+    },
+  },
+  typography: {
+    fontFamily: 'Arial, sans-serif', // Global font
+    h1: {
+      fontSize: '2.5rem',
+      fontWeight: 600,
+
+    },
+    body1: {
+      fontSize: '1rem',
+      lineHeight: 1.5,
+    },
+    button: {
+      textTransform: 'none',
+    },
+  },
+  
+});
+
+export default theme;
